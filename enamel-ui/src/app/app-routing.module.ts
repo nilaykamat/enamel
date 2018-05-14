2ffab56242bd7336482b9d5dcac05d68c818064e
Amey Naiyan <ameyd9@gmail.com>
2018-05-14 09:43:06 +0530

    Removed components scaffolding for restructuring

--- enamel-ui/src/app/app-routing.module.ts
+++ enamel-ui/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { LoginComponent } from './user/login/login.component';
+//import { DashboardComponent } from './user/dashboard/dashboard.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+	// {
+	//     path: '',
+	//     component: LoginComponent,
+	// },
+	// {
+	//     path: 'welcome',
+	//     component: DashboardComponent,
+	// }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- enamel-ui/src/app/app.module.ts
+++ enamel-ui/src/app/app.module.ts
@@ -4,47 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
-import { AdminComponent } from './user/settings/admin/admin.component';
-import { SettingsComponent } from './user/settings/settings.component';
-import { AccountComponent } from './user/settings/account/account.component';
-import { DashboardComponent } from './user/dashboard/dashboard.component';
-import { SidebarComponent } from './common/sidebar/sidebar.component';
-import { FooterComponent } from './common/footer/footer.component';
-import { HeaderComponent } from './common/header/header.component';
-import { AddComponent } from './patient/add/add.component';
-import { DetailsComponent } from './patient/details/details.component';
-import { ListComponent } from './patient/list/list.component';
-import { PaymentsComponent } from './patient/payments/payments.component';
-import { AppointmentsComponent } from './patient/appointments/appointments.component';
-import { CalenderComponent } from './appointments/calender/calender.component';
-import { InteractiveComponent } from './patient/interactive/interactive.component';
-import { VisitsComponent } from './patient/visits/visits.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ForgotPasswordComponent,
-    AdminComponent,
-    SettingsComponent,
-    AccountComponent,
-    DashboardComponent,
-    SidebarComponent,
-    FooterComponent,
-    HeaderComponent,
-    AddComponent,
-    DetailsComponent,
-    ListComponent,
-    PaymentsComponent,
-    AppointmentsComponent,
-    CalenderComponent,
-    InteractiveComponent,
-    VisitsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
