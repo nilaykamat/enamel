32962028c8987ea13d52fa7cb130e6dd5b9ddf4c
nilay <nilay@browntape.com>
2018-05-25 15:33:22 +0530

    Added sample API routes for testing

--- enamel-api/server.js
+++ enamel-api/server.js
@@ -9,6 +9,26 @@ var port = process.env.PORT || 8080;        // set our port
 
 var router = express.Router();              // get an instance of the express Router
 
+var users = [
+	{name : 'nilay', status: true},
+	{name : 'abhijit', status: true},
+	{name : 'keegan', status: false},
+	{name : 'mohit', status: true},
+	{name : 'kalpesh', status: false},
+];
+router.use(function(req, res, next) {
+	// console.log(req.headers);
+	console.log('MiddleWare for authentication');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.route('/users').get((req, res) => {
+	res.json(users);
+}).post((req, res) => {
+	users.push(req.body);
+	res.json({message: 'Welcome to enamel API'});
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'Welcome to enamel API' });   
