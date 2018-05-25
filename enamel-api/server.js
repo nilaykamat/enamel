f7c738193809b413eefd4881b68da83b405e88ec
nilay <nilay@browntape.com>
2018-05-25 13:52:20 +0530

    Added basic expressJs api functionality

--- enamel-api/server.js
+++ enamel-api/server.js
@@ -1,8 +1,21 @@
-var http = require('http');
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
 
-http.createServer(function(request, respone){
-  respone.writeHead(200, {'Content-type':'text/plan'});
-  response.write('Hello Node JS Server Response');
-  response.end( );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-}).listen(7000);
+var port = process.env.PORT || 8080;        // set our port
+
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to enamel API' });   
+});
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+app.listen(port);
+console.log('Server running on port:' + port + '. Go to http://localhost:8080/api/');
